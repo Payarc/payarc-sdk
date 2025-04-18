80064a15a18bfe72c1cac3e21bd8c5a70e52588e
shumbatov <104630120+shumbatov@users.noreply.github.com>
2025-04-18 13:13:00 -0400

    Refactor request headers in Payarc SDK and update test script for bet… (#4)

--- tests/payarcConnectRunner.js
+++ tests/payarcConnectRunner.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const Payarc = require("../index");
+//const Payarc = require("../testcode");
 
 const bearerToken = process.env.PAYARC_KEY;
 const baseUrl = process.env.PAYARC_ENV;
+const bearerTokenAgent = process.env.PAYARC_AGENTKEY;
+const accountListExistingBearerToken = process.env.PAYARC_ACCOUNTLISTEXISTINGKEY;
+const payarcDisputeCaseToken = process.env.PAYARC_DISPUTECASEKEY;
+const payarcAgentWithSubAgentCaseToken = process.env.PAYARC_AGENTWITHSUBAGENTKEY;
+const payarcConnectAccessToken = process.env.PAYARC_PAYARCCONNECTKEY;
 const apiVersion = "/v1/";
 const version = "1.0";
-const bearerTokenAgent = null;
 
 const payarc = new Payarc(
   bearerToken,
@@ -15,122 +20,24 @@ const payarc = new Payarc(
   bearerTokenAgent
 );
 
-async function login() {
-    await payarc.payarcConnect
-    .login()
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function sale() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .sale(tenderType = "CREDIT", ecrRefNum = "REF99", amount = "99", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function pcVoid() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .void(payarcTransactionId = "WBMROoRyXoMRXOyn", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function refund() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .refund(amount = "100", payarcTransactionId = "WBMROoRyXoMRXOyn", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function blindCredit() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .blindCredit(ecrRefNum = "Ref11", amount = "15", token = "IYmDAxNtma7g5228", expDate = "0227", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function auth() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .auth(ecrRefNum = "Ref11", amount = "111", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function postAuth() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .postAuth(ecrRefNum = "Ref12", origRefNum = "25", amount = "10", deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function lastTransaction() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .lastTransaction(deviceSerialNo = "1850406725")
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function serverInfo() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .serverInfo()
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
-}
-async function terminals() {
-    await payarc.payarcConnect
-    .login()
-    .catch((error) => console.error("Error:", error));
-    payarc.payarcConnect
-    .terminals()
-    .then((result) => {
-      console.log("Result", result);
-    })
-    .catch((error) => console.error("Error:", error));
+const payarcListExisting = new Payarc(
+  payarcConnectAccessToken,
+  baseUrl,
+  apiVersion,
+  version,
+  accountListExistingBearerToken
+);
+
+async function test() {
+  payarc.customers.list({
+    limit: 3
+})
+.then((response) => {
+    const { customers = [] } = response;
+    console.log(customers[0].card.data);
+})
+.catch(error => console.error(error));
+  //JSON.stringify(result, null, '\t')
 }
 
-// login()
-// sale()
-// pcVoid()  
-// refund()
-// blindCredit()
-// auth()
-// postAuth()
-// lastTransaction()
-// serverInfo()
-terminals()
-
+test();
