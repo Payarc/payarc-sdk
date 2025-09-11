7d51ef0cc5597d4d2e3674dd7df95abba0dacb79
shumbatov <104630120+shumbatov@users.noreply.github.com>
2025-09-11 10:25:47 -0400

    Refactor test function to remove commented-out code and implement instructional funding creation

--- tests/payarcConnectRunner.js
+++ tests/payarcConnectRunner.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const Payarc = require("../index");
 //const Payarc = require("../testcode");
-
 const bearerToken = process.env.PAYARC_KEY;
 const baseUrl = process.env.PAYARC_ENV;
 const bearerTokenAgent = process.env.PAYARC_AGENTKEY;
@@ -11,7 +10,6 @@ const payarcAgentWithSubAgentCaseToken = process.env.PAYARC_AGENTWITHSUBAGENTKEY
 const payarcConnectAccessToken = process.env.PAYARC_PAYARCCONNECTKEY;
 const apiVersion = "/v1/";
 const version = "1.0";
-
 const payarc = new Payarc(
   bearerToken,
   baseUrl,
@@ -19,7 +17,6 @@ const payarc = new Payarc(
   version,
   bearerTokenAgent
 );
-
 const payarcListExisting = new Payarc(
   payarcConnectAccessToken,
   baseUrl,
@@ -29,63 +26,15 @@ const payarcListExisting = new Payarc(
 );
 
 async function test() {
-
-
-  // payarc.charges.list()
-  //   .then((response) => {
-  //     const { charges = {} } = response;
-  //     console.log(JSON.stringify(charges, null, '\t'));
-  //   })
-  //   .catch(error => console.error(error));
-
-// payarc.charges.retrieve('6dl8k07950g9ymxw')
-//   .then(charge => console.log('Success the charge is ', JSON.stringify(charge, null, '\t')))
-//   .catch(error => console.error('Error detected:', error));
-
-
-  // payarc.charges.create({
-  //   amount: 10, // Amount in cents
-  //   currency: 'usd', // Currency code (e.g., 'usd')
-  //   source: {
-  //     card_number: '4085404032505228', // Payment source (e.g., credit card number)
-  //     exp_month: '02',  //Credit card attributes 
-  //     exp_year: '2027', //Credit card attributes 
-  //   },
-  //   splits: [
-  //     {
-  //       mid: "0709900000098856",
-  //       percent: 30,
-  //     },
-  //     {
-  //       mid: "0709900000098856",
-  //       amount: 15,
-  //     }
-  //   ]
-  // })
-  //   .then(charge => console.log('Success the charge is ', JSON.stringify(charge, null, '\t')))
-  //   .catch(error => console.error('Error detected:', error));
-
-
-
-    // payarc.instructionalFunding.list()
-    // .then((response) => {
-    //   console.log(JSON.stringify(response, null, '\t'));
-    // })
-    // .catch(error => console.error(error));
-
-
-    payarc.instructionalFunding.create(
-      {
-        mid: "0709900000098856",
-        amount: 500,
-      }
-    )
-    .then((response) => {
-      console.log(JSON.stringify(response, null, '\t'));
-    })
-    .catch(error => console.error(error));
-
-
+  payarc.batches.listByAgent({
+    from_date: '2024-09-01',
+    to_date: '2024-09-05'
+  })
+.then((response) => {
+    const { charges = {} } = response;
+    console.log(response);
+})
+.catch(error => console.error(error));
   //JSON.stringify(result, null, '\t')
 }
 
@@ -100,5 +49,4 @@ async function test() {
 // .catch(error => console.error(error));
 //   //JSON.stringify(result, null, '\t')
 // }
-
 test();
